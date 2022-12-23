9c963de451a951218072729a644776c2276263bb
vmion <ohj106992@gmail.com>
2022-12-23 18:49:12 +0900

    _12_23_002
    Ground

--- Assets/Script/Character/Char_ani.cs
+++ Assets/Script/Character/Char_ani.cs
@@ -18,8 +18,7 @@ public class Char_ani :  MonoBehaviour
     public Image JoyStick_Lever;
     void Start()
     {
-        ani = Char.GetComponent<Animator>();
-        Char.gameObject.SetActive(true);
+        ani = Char.GetComponent<Animator>();        
         Char.transform.position = new Vector3(-15f, 0, 0);
         moveSpeed = 3f;
         centerPos = JoyStick.rectTransform.position;        
@@ -37,7 +36,7 @@ public class Char_ani :  MonoBehaviour
             Vector3 lookforward = new Vector3(Char.forward.x, 0f, Char.forward.z).normalized;
             Vector3 lookRight = new Vector3(Cam.right.x, 0f, Cam.right.z).normalized;
             Vector3 moveDir = lookforward * moveInput.y + lookRight * moveInput.x;
-            Char.forward = lookforward;
+            Char.forward = lookforward;            
             transform.position += moveDir * moveSpeed * Time.deltaTime;
                                
             if (hp.fillAmount == 0)

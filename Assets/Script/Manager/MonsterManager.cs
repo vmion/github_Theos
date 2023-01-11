172589744a2fec003832e847d80c618e37a5737c
vmion <ohj106992@gmail.com>
2023-01-11 22:05:51 +0900

    _01_11_002
    Clamp

--- Assets/Script/Manager/MonsterManager.cs
+++ Assets/Script/Manager/MonsterManager.cs
@@ -8,6 +8,7 @@ public class MonsterManager : MonoBehaviour
     private static MonsterManager Instance;
     public static Dictionary<string, GameObject> mobDic;
     public Transform ParentMonster;
+    public static List<Vector3> CenterList;
     Collider otherCollider;
     public static MonsterManager instance
     {
@@ -29,8 +30,7 @@ public class MonsterManager : MonoBehaviour
         mobDic.Add("사티르", tmpObjs[2]);
         mobDic.Add("아라크네", tmpObjs[3]);
         mobDic.Add("미노타우루스", tmpObjs[4]);
-        
-              
+        CenterList = new List<Vector3>();
     }
     void Start()
     {
@@ -62,27 +62,31 @@ public class MonsterManager : MonoBehaviour
         {
             int nR = i / column;
             int nC = i % column;
-            Vector3 centerPos = GetCellCenterPos(nR, nC);            
+            Vector3 centerPos = GetCellCenterPos(nR, nC);
+            CenterList.Add(centerPos);
             if (i % 3 == 0)
             {
                 GameObject mob = Instantiate(MonsterManager.mobDic["켄타우로스"], ParentMonster);
                 mob.tag = "Monster";
                 mob.transform.position = centerPos;
-                mob.AddComponent<Monster_ani>();                
+                mob.AddComponent<Monster_ani>();
+                mob.transform.localPosition = Monster_ani.ClampPosition(centerPos);
             }
             else if (i % 3 == 1)
             {
                 GameObject mob = Instantiate(MonsterManager.mobDic["고르곤"], ParentMonster);
                 mob.tag = "Monster";
                 mob.transform.position = centerPos;
-                mob.AddComponent<Monster_ani>();                
+                mob.AddComponent<Monster_ani>();
+                mob.transform.localPosition = Monster_ani.ClampPosition(centerPos);
             }
             else
             {
                 GameObject mob = Instantiate(MonsterManager.mobDic["사티르"], ParentMonster);
                 mob.tag = "Monster";
                 mob.transform.position = centerPos;
-                mob.AddComponent<Monster_ani>();                
+                mob.AddComponent<Monster_ani>();
+                mob.transform.localPosition = Monster_ani.ClampPosition(centerPos);
             }
         }
     }
--- Assets/Script/Monster/Monster_ani.cs
+++ Assets/Script/Monster/Monster_ani.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class Monster_ani : MonoBehaviour
 {
     Animator ani;
+    Dictionary<int, Vector3> centerDic;
+    public static Vector3 distance;
     void Awake()
     {
         ani = GetComponent<Animator>();
+        centerDic = new Dictionary<int, Vector3>();
+        for(int i = 0; i < MonsterManager.CenterList.Count; i++)
+        {
+            Vector3 pos = MonsterManager.CenterList[i];
+            centerDic.Add(i, pos);
+        }
+        distance = (centerDic[0] - centerDic[5]) / 2;
     }
     void Start()
     {
@@ -16,5 +25,17 @@ public class Monster_ani : MonoBehaviour
     void Update()
     {
         
+    }
+    void Move()
+    {
+        
+    }
+    public static Vector3 ClampPosition(Vector3 _position)
+    {
+        Vector3 clampPos;
+        clampPos.x = Mathf.Clamp(_position.x, -distance.x, distance.x);
+        clampPos.y = 0f;
+        clampPos.z = Mathf.Clamp(_position.z, -distance.z, distance.z);
+        return clampPos;
     }
 }
